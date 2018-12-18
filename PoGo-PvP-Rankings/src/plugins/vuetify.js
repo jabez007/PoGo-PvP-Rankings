19b25686aabf936c48e319d39fc5ae7141382f5b
Jimmy <jabez007@users.noreply.github.com>
2018-12-18 14:53:20 -0600

    type colors

--- PoGo-PvP-Rankings/src/plugins/vuetify.js
+++ PoGo-PvP-Rankings/src/plugins/vuetify.js
@@ -2,6 +2,27 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
 
+// https://bulbapedia.bulbagarden.net/wiki/Category:Type_color_templates
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    bug: '#A8B820',
+    dark: '#705848',
+    dragon: '#7038F8',
+    electric: '#F8D030',
+    fairy: '#EE99AC',
+    fighting: '#C03028',
+    fire: '#F08030',
+    flying: '#A890F0',
+    ghost: '#705898',
+    grass: '#78C850',
+    ground: '#E0C068',
+    ice: '#98D8D8',
+    normal: '#A8A878',
+    poison: '#A040A0',
+    psychic: '#F85888',
+    rock: '#B8A038',
+    steel: '#B8B8D0',
+    water: '#6890F0',
+  }
 });
