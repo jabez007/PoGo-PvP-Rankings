15d91f8cbbe6c40cd53ce468993f4c8d073ecb89
Jimmy <jabez007@users.noreply.github.com>
2018-12-20 13:33:22 -0600

    Create PokemonStatsCard.vue

---
+++ PoGo-PvP-Rankings/src/components/PokemonStatsCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card height="100%"
+          flat
+          tile>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PokemonStatsCard',
+  props: {
+    types: {
+      pokemon: String,
+      required: true,
+    },
+  },
+  components: {
+    // TypeChip: () => import('@/components/TypeChip.vue'),
+  },
+  data: () => ({
+    stats: {
+      attack: 0,
+      special_attack: 0,
+      defense: 0,
+      special_defense: 0,
+      speed: 0,
+      hp: 0,
+    },
+  })
+  computed: {
+    speed() {
+      return 1 + ((this.stats.speed - 75) / 500);
+    },
+    attack() {
+      return Math.round(this.scaled(this.stats.attack, this.stats.special_attack) * this.speed);
+    },
+    defense() {
+    return Math.round(this.scaled(this.stats.defense, this.stats.special_defense) * this.speed);
+    },
+    stamina() {
+      return 2 * this.stats.hp;
+    },
+  },
+  created() {
+    const self = this;
+    this.$pokedex.getPokemonByName(this.pokemon.toLowerCase())
+      .then((response) => {
+        response.stats
+          .forEach((s) => {
+            self.stats[s.stat.name.replace('-','_')] = s.base_stat;
+          });
+      });
+  },
+  methods: {
+    scaled(physical, special) {
+      return Math.round(2 * ((7 / 8) * Math.max(physical, special) + (1 / 8) * Math.min(physical, special)));
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
