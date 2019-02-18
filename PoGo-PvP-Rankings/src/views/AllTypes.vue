1eeaee625f6cec4d6fdd77cbf7223b956b021c30
Jimmy <jabez007@users.noreply.github.com>
2019-02-18 12:14:48 -0600

    Create AllTypes.vue

---
+++ PoGo-PvP-Rankings/src/views/AllTypes.vue
@@ -0,0 +1,13 @@
+<template>
+  <TypesRanking>
+  </TypesRanking>
+</template>
+
+<script>
+export default {
+  name: 'AllTypes',
+  components: {
+    TypesRanking: () => import('@/components/TypesRanking.vue'),
+  },
+}
+</script>
