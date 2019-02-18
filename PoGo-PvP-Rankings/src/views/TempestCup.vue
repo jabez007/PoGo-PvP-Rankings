c45f0318940ffd0db0fe393a10c1ccebb25adca1
Jimmy <jabez007@users.noreply.github.com>
2019-02-18 12:31:15 -0600

    Create TempestCup.vue

---
+++ PoGo-PvP-Rankings/src/views/TempestCup.vue
@@ -0,0 +1,13 @@
+<template>
+  <TypesRanking :requestedTypes="['electric', 'ice', 'flying', 'ground']">
+  </TypesRanking>
+</template>
+
+<script>
+export default {
+  name: 'AllTypes',
+  components: {
+    TypesRanking: () => import('@/components/TypesRanking.vue'),
+  },
+}
+</script>
