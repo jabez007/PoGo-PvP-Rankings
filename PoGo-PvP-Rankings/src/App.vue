2fb8e026ab241674c3d07ec7883244eefc85d1d9
Jimmy <jabez007@users.noreply.github.com>
2019-02-18 12:58:33 -0600

    Update App.vue

--- PoGo-PvP-Rankings/src/App.vue
+++ PoGo-PvP-Rankings/src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <v-app id="app"
-         dark>
-    <v-navigation-drawer id="nav"
-                         v-model="drawer"
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer"
                          clipped
                          fixed
                          app>
+      <v-list three-line 
+              dense>
+        <v-list-group prepend-icon=""
+                      :value="true"
+                      no-action
+                      lazy>
+          <v-list-tile slot="activator">
+            <v-list-tile-content>
+              <v-list-tile-title>Silph Arena Cups</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/cups/boulder"
+                       ripple>
+            <v-list-tile-content>
+              <v-list-tile-title>Boulder Cup</v-list-tile-title>
+              <v-list-tile-sub-title>January</v-list-tile-title>
+              <v-list-tile-sub-title>Rock, Ground, Steel, Fighting</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/cups/twilight"
+                       ripple>
+            <v-list-tile-content>
+              <v-list-tile-title>Twilight Cup</v-list-tile-title>
+              <v-list-tile-sub-title>February</v-list-tile-title>
+              <v-list-tile-sub-title>Fairy, Dark, Poison, Ghost</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/cups/tempest"
+                       ripple>
+            <v-list-tile-content>
+              <v-list-tile-title>Tempest Cup</v-list-tile-title>
+              <v-list-tile-sub-title>March</v-list-tile-title>
+              <v-list-tile-sub-title>Electric, Ice, Flying, Ground</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-group>
+      </v-list>
     </v-navigation-drawer>
     <v-toolbar clipped-left
                fixed
@@ -14,10 +49,10 @@
       <v-toolbar-title>PoGo PvP Rankings</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <!--v-btn to="/"
-                  flat>
+        <v-btn to="/"
+               flat>
           Home
-        </v-btn-->
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
