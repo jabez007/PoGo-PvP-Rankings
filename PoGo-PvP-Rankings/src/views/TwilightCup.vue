3690c26b9722fba006a75846b0ddd59299d7ec62
Jimmy <jabez007@users.noreply.github.com>
2019-02-18 12:29:43 -0600

    Create TwilightCup.vue

---
+++ PoGo-PvP-Rankings/src/views/TwilightCup.vue
@@ -0,0 +1,13 @@
+<template>
+  <TypesRanking :requestedTypes="['fairy', 'dark', 'poison', 'ghost']">
+  </TypesRanking>
+</template>
+
+<script>
+export default {
+  name: 'AllTypes',
+  components: {
+    TypesRanking: () => import('@/components/TypesRanking.vue'),
+  },
+}
+</script>
