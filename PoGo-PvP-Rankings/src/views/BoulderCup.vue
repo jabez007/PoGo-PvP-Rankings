938f66f8a883becc00c0251d1499a1a8077a64e7
Jimmy <jabez007@users.noreply.github.com>
2019-02-18 12:28:14 -0600

    Create BoulderCup.vue

---
+++ PoGo-PvP-Rankings/src/views/BoulderCup.vue
@@ -0,0 +1,13 @@
+<template>
+  <TypesRanking :requestedTypes="['rock', 'ground', 'steel', 'fighting']">
+  </TypesRanking>
+</template>
+
+<script>
+export default {
+  name: 'AllTypes',
+  components: {
+    TypesRanking: () => import('@/components/TypesRanking.vue'),
+  },
+}
+</script>
